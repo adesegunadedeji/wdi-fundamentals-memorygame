56c395dc503e8f13afad4f2a520a9fb6e9217892
Adesegun Adedeji <adesegunadedeji@Adeseguns-MacBook-Pro.local>
2019-05-10 14:40:06 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+/* Link  the index.html to the main file  by copying the script elements s
+script element and placing it in the html file right before the body closing tag 
+<script src="js/main.js"></script> */
+console.log("Up and Running!!");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log ("User flipped " + cardOne);
+console.log ("User flipped " + cardFour);

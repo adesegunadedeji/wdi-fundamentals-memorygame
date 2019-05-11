2b33139f2d48fed0ae1b79cc567efbdadd3867af
Adesegun Adedeji <adesegunadedeji@Adeseguns-MacBook-Pro.local>
2019-05-11 11:30:02 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,10 +2,27 @@
 script element and placing it in the html file right before the body closing tag 
 <script src="js/main.js"></script> */
 console.log("Up and Running!!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log ("User flipped " + cardOne);
-console.log ("User flipped " + cardFour);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log ("User flipped " + cardsInPlay[0]);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log ("User flipped " + cardsInPlay[1]);
+
+if(cardsInPlay.length === 2){
+console.log("The Length Equals two");
+if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You Found a match"); 
+	}
+	else{
+		alert("Sorry Try Again");
+	}}
+	else{ 
+		alert("Sorry Try Again");
+	}
+
+
+

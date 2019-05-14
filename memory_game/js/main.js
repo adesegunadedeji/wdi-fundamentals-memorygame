9b2ea64b20cb27048e155c2b82498687550dc106
Adesegun Adedeji <adesegunadedeji@Adeseguns-MacBook-Pro.local>
2019-05-13 23:47:01 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,28 +25,53 @@ var cards = [
 	cardImage: "images/king-of-diamonds.png",
 }
 ];
+//Array with cards which are in play: 
 var cardsInPlay = [];
+//Function that checks for Match
 var checkForMatch = function(){
-
+	//THIS IS THE PART IM STRUGGLING WITH 
+	this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay[0] === cardsInPlay[1]){
-	console.log("You Found a match"); 
+	alert("You Found a match"); 
 	}
 	else{
 	alert("Sorry Try Again");
 	}
 }
-var flipCard = function(cardId) {
+
+
+var flipCard = function () {
+var cardId = this.getAttribute("data-id");
+
 console.log("User flipped " + cards[cardId].rank);
 cardsInPlay.push(cards[cardId].rank);
 
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 
-checkForMatch();
+checkForMatch(this);
 }
-flipCard(0);
-flipCard(2);
 
+
+var createBoard = function(){
+for (var i = 0; i < cards.length; i++){
+	var cardElement = document.createElement('img');
+
+	cardElement.setAttribute("src", "images/back.png");
+	cardElement.setAttribute("data-id",i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById("game-board").appendChild(cardElement);
+}
+	
+
+}
+
+/*function resetButton() {
+	document.getElementById("myForm").reset();
+}*/
+/*<form id="myForm"> <input type="button" onclick="resetButton()" value="Reset the Game">*/
+
+createBoard();
 
 
 
